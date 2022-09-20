3a3a9fb51749816b7c6071fff67bc341b1024ad4
Swipez1337 <71495304+Swipez1337@users.noreply.github.com>
2022-09-20 13:18:11 +0200

    Update App.test.js

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,17 +1,18 @@
 
-/*import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/*
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
+*/
 test('has login button', () => {
   render(<App />);
   let button = screen.getByRole("button");
-  expect(button).toHaveTextContent("Login");
+  expect(button).toHaveTextContent("Log ind");
 });
 
-*/
+
