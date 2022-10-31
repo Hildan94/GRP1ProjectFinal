f8721650e395c693655d2893b8f509f2894958e2
Hildibjørg Didriksen <s164539@student.dtu.dk>
2022-10-31 16:10:41 +0100

    Updated BaseUrl for db, cleartext for login db

--- src/main/java/DB/HibernateController.java
+++ src/main/java/DB/HibernateController.java
@@ -10,8 +10,8 @@ public class HibernateController {//Should be a singleton…
     public HibernateController(String dbUrl){
         Configuration configuration = new Configuration(); //NB org.hibernate.cfg.Configuration
         configuration.addAnnotatedClass(User.class); //remember to do this for all DB entities
-        configuration.setProperty("hibernate.connection.username", System.getenv("devopse22user"));
-        configuration.setProperty("hibernate.connection.password", System.getenv("devopse22pass"));
+        configuration.setProperty("hibernate.connection.username","pg");
+        configuration.setProperty("hibernate.connection.password", "889226e9a74ce148");
         configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://" + dbUrl);
         configuration.setProperty("hibernate.hbm2ddl.auto","update"); //update Schema - don't do this in prod
         this.sessionFactory = configuration.buildSessionFactory();
