7abb00483c575a466991456ad942f7611321bb0b
ASBP <s194582@dtu.dk>
2022-11-02 21:20:31 +0100

    Changes in signup.js

--- web/src/pages/signup.js
+++ web/src/pages/signup.js
@@ -1,55 +1,52 @@
 import {useNavigate} from "react-router-dom";
 import {React} from "react";
+import {Button, TextField} from "@mui/material";
 
 function Signup() {
 
     const navigate = useNavigate()
 
-    const toHome = () => {
+    const toHome = () => { // Startside
         navigate('/home')
     }
 
-    const toFrontpage = () => {
+    const toFrontpage = () => { // Loginside
         navigate('/')
     }
 
     return (
-        <div>
+        <div style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+            <div>
+                <h1>Opret ny bruger</h1>
+            </div>
             <div>
                 <form>
                     <label>
-                        Brugernavn
-                        <input type="text" name="username"/>
+                        <input type="text" placeholder="  Fornavn" name="name"/>
+                        <input type="text" placeholder="  Efternavn" name="name"/>
                     </label>
                 </form>
                 <form>
                     <label>
-                        Fulde navn
-                        <input type="text" name="name"/>
+                        <input type="text" placeholder="   Skole" name="school"/>
                     </label>
                 </form>
                 <form>
                     <label>
-                        Skole
-                        <input type="text" name="school"/>
+                        <input type="text" placeholder="   Klassetrin" name="gradeno"/>
                     </label>
                 </form>
                 <form>
                     <label>
-                        Klassetrin
-                        <input type="text" name="gradeno"/>
+                        <input type="text" placeholder="   Kodeord" name="password"/>
                     </label>
                 </form>
                 <form>
                     <label>
-                        Kodeord
-                        <input type="text" name="password"/>
-                    </label>
-                </form>
-                <form>
-                    <label>
-                        Gentag kodeord
-                        <input type="text" name="passwordconfirm"/>
+                        <input type="text" placeholder="   Bekræft kodeord" name="passwordconfirm"/>
                     </label>
                 </form>
             </div>
@@ -57,11 +54,17 @@ function Signup() {
                 <button onClick={toHome}>Opret bruger</button>
             </div>
             <div>
-                <button onClick={toFrontpage}>Tilbage</button>
+                <button onClick={toFrontpage}>Allerede oprettet?</button>
             </div>
+
         </div>
     );
-
 }
 
 export default Signup;
+
+/*
+<div className="link">
+    <a href="/">Allerede oprettet?</a>
+</div>
+* */
