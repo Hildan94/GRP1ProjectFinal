971aad1d6e542f5c05ee52cb6835cf8ba5cf9840
Troels <troelstoy>
2022-11-02 17:29:12 +0100

    Added new branch, and made minor changes

--- web/src/pages/reports.js
+++ web/src/pages/reports.js
@@ -5,31 +5,27 @@ function Reports() {
 
     const navigate = useNavigate()
 
-    const toQuizzes = () => {
-        navigate('/quizzes')
-    }
-
     const toHome = () => {
         navigate('/home')
     }
+
+
 
     return (
         <div>
             <div>
                 <h1 onClick={toHome}> NEM Læringsplatform </h1>
+                <button> Hjem</button>
             </div>
             <div>
-                <h1> VALGT FAG </h1>
-                <h2> Vælg den ønskede quiz at generere rapport ud fra</h2>
+                <h1> Matematik </h1>
+                <h2> Tryk på den ønskede quiz at generere rapport ud fra</h2>
             </div>
             <div>
-                <h3> Quiz 1 blok</h3>
-                <h3> Quiz 2 blok</h3>
-                <h3> Quiz 3 blok</h3>
-                <h3> Quiz 4 blok</h3>
+                <button> Quiz 1 blok</button>
             </div>
             <div>
-                <button onClick={toQuizzes}>Generer rapport</button>
+                <button>Generer rapport</button>
             </div>
         </div>
     );
