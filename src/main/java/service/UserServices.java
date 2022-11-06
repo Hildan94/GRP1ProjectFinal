9f3c2b6df2093d603b53d1745415160636a144d8
SAA-DK <sebastian_almfort@hotmail.com>
2022-11-06 16:16:19 +0100

    Kæmper meget med Backend. Kigget lidt op UserService.java og UserServices.java. Hvis dette chrasher main, så gå tilbage til tidligere versioner fra mig.

--- src/main/java/service/UserServices.java
+++ src/main/java/service/UserServices.java
@@ -2,27 +2,50 @@ package service;
 
 import DB.HibernateController;
 import DB.User;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-public class UserServices {
-    HibernateController hibernateController = new HibernateController("pgtest-db.caprover.grp1.diplomportal.dk:6543/pg");
+import java.util.List;
 
-    void fetch() {
-        SessionFactory sessionFactory = hibernateController.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        User user = new User();
-        System.out.print("UserID before commit: " + user.getId());
-        user.setUsername("BenteBent");
-        session.persist(user);
-        transaction.commit();
-        System.out.println("UserID after commit: " + user.getId());
-        Transaction readTransaction = session.beginTransaction();
-        User readUser = session.get(User.class, user.getId());
-        System.out.println("Read user back: " + readUser.toString());
-        readTransaction.commit();
-        session.close();
+/* Opgaver fra uge 8 der arbejdes med her */
+
+@Path("users")
+public class UserServices {
+
+    @GET
+    @Path("query")
+    public List<User> queryUsers(@QueryParam("name") String name) throws
+            NoImplementationException {
+    //No implementation yet
+        throw new NoImplementationException("user-queries not implemented, yet");
     }
+
+    public class NoImplementationException extends Exception {
+        public NoImplementationException(String s) {
+            super(s);
+        }
+    }
+
+    @Provider
+    public class NoImplementationExceptionMapper implements
+            ExceptionMapper<NoImplementationException> {
+        @Override
+        public Response toResponse(NoImplementationException e) {
+            return
+                    Response.status(Response.Status.NOT_IMPLEMENTED).entity(e.getMessage()).
+                            build();
+        }
+    }
+
+
+
+
 }
+
--- web/src/Backend/frontpage.java
+++ web/src/Backend/frontpage.java
@@ -2,9 +2,11 @@
 
 package Backend
 
-
+/* Service der kan finde brugere ud fra deres navn eks. */
 @Path ("users")
 public class frontpage {
+
+
 
     @GET
     @Path("query")
@@ -14,7 +16,7 @@ public class frontpage {
         throw new NoImplementationException("user-queries not implemented, yes");
     }
 
-
+    /* Bruges en ny exception */
     public class NoImplementationException extends Exception {
         public NoImplementationException(String s) {
             super(s);
@@ -22,89 +24,16 @@ public class frontpage {
     }
 
     @Provider
-    public class NoImplementationExceptionMapper implements ExceptionMapper<NoImplementationException> {
-
+    public class NoImplementationExceptionMapper implements
+            ExceptionMapper<NoImplementationException> {
         @Override
         public Response toResponse(NoImplementationException e) {
-            return Response.status(Response.Status.NOT_IMPLEMENTED).entity(e.getMessage()).build();
-        }
-
-        /* JWT-Controller-klasse, der kan udstede JWT-tokens. */
-        public class JWTHandler {
-            private static String key = "hæmli";
-            private static final int TOKEN_EXPIRY = 2880; //2 days
-
-            public static String generateJwtToken(User user) {
-                Calendar expiry = Calendar.getInstance();
-                expiry.add(Calendar.MINUTE, TOKEN_EXPIRY);
-                ObjectMapper objectMapper = new ObjectMapper();
-                try {
-                    String s = objectMapper.writer().writeValueAsString(user);
-                    return JWT.create()
-                            .withIssuer("GiraffeDeluxe")
-                            .withClaim("user", s)
-                            .withExpiresAt(expiry.getTime())
-                            .sign(Algorithm.HMAC512(key));
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-
-                public static User validate (String s){
-                    JWTVerfier verifier = JWT.require(Algorithm.HMAC512(key)).build();
-                    DecodedJWT verify = verifier.verify(s);
-                    Claim user = verify.getClaim("user");
-                    try {
-                        return new objectMapper().reader().forType(User.class).readValue(user.asString());
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        }
-
-        /* endpoint til at uddele tokens */
-        /* endpoint til at uddele tokens */
-        @Path("login")
-        @Produces(MediaType.APPLICATION_JSON)
-        @Consumes(MediaType.APPLICATION_JSON)
-        public class LoginService {
-
-            @POST
-            public String postLoginData(loginData login) throws NotAuthorizedException
-            {
-                if (login!=null && "brian".equals(login.getUsername()) &&
-                        "kodeord".equals(login.getPassword())){
-                    return JWTHandler.generateJwtToken(new
-                            User(login.getUsername(), ""));
-            }
-            throw new NotAuthorizedException("forkert brugernavn/kodeord");
-        }
-
-    }
-    public class NotAuthorizedException extends Throwable {
-        public NotAuthorizedException(String s) {
-            super(s);
-        }
-    }
-    }
-
-    @Provider
-    public class NotAuthorizedException implements ExceptionMapper<NotAuthorizedException> {
-
-        @Override
-        public Response toResponse(NotAuthorizedException e) {
-            return Response.status(Response.Status.UNAUTHORIZED).entity(e.getMessage()).build();
+            return
+                    Response.status(Response.Status.NOT_IMPLEMENTED).entity(e.getMessage()).
+                            build();
         }
     }
 
-    /*  data-klasse til login data */
-    @Data
-    @NoArgsConstructor
-    public class LoginData {
-        private string username;
-        private String password;
-    }
 }
-
 
 
