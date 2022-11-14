ff30366144bb85623c4b669ed0857e63badc4602
Troels Christoffersen <s152780@student.dtu.dk>
2022-11-14 16:33:44 +0100

    Now shows reports in table

--- web/src/pages/Reports/ReportsOverview.js
+++ web/src/pages/Reports/ReportsOverview.js
@@ -4,6 +4,7 @@ import {observer} from "mobx-react-lite";
 import {scores} from "./ReportsFetcher";
 import {observable} from "mobx";
 
+
 function ReportsOverview() {
 
     let reportId = 'nan'
@@ -14,8 +15,14 @@ function ReportsOverview() {
         navigate('report/' + key.toString())
     }
 
+    /**
+     * Skal udfases
+     */
     const myList = observable(scores.scoresString.map((scoreName,key)=>
     <h2> Quiz {key+1} <button onClick={e => toReport(key)}> <li key = {key}>{scoreName}</li></button></h2>))
+
+    const myListtable = observable(scores.scoresString.map((Scorename, key) =>
+        <tr> <td>Quiz {key+1} </td> <td> {24 + key} / 50 </td> <td onClick={e=> toReport(key)}> Se resultat </td> </tr>))
 
     const navigate = useNavigate()
 
@@ -28,11 +35,15 @@ function ReportsOverview() {
             <div>
                 <h1>NEM Læringsplatform </h1>
                 <button onClick={toHome}> Hjem</button>
-            </div>
             <div>
                 <h1> Matematik resultater</h1>
                 <h2>  {myList}  </h2>
                 <h3> Tryk på den ønskede quiz at se rapport ud fra</h3>
+            </div>
+                <h1> Matematik</h1>
+                <table>
+                    {myListtable}
+                </table>
             </div>
         </div>
     );
