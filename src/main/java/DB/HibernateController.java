fc743fba7f24ed8a78fdb73e98e3686d0ac59ce4
Troels Christoffersen <s152780@student.dtu.dk>
2022-11-22 08:33:56 +0100

    Trying to fix online db acess

--- src/main/java/DB/HibernateController.java
+++ src/main/java/DB/HibernateController.java
@@ -11,8 +11,10 @@ public class HibernateController {//Should be a singleton…
         configuration.addAnnotatedClass(User.class); //remember to do this for all DB entities
         configuration.addAnnotatedClass(Report.class);
         configuration.addAnnotatedClass(Questions.class);
-        configuration.setProperty("hibernate.connection.username",System.getenv("devopse22user"));
-        configuration.setProperty("hibernate.connection.password",System.getenv("devopse22pass"));
+        //configuration.setProperty("hibernate.connection.username",System.getenv("devopse22user"));
+        //configuration.setProperty("hibernate.connection.password",System.getenv("devopse22pass"));
+        configuration.setProperty("hibernate.connection.username",System.getenv("POSTGRES_USER"));
+        configuration.setProperty("hibernate.connection.password",System.getenv("POSTGRES_PASSWORD"));
         configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://" + dbUrl);
         configuration.setProperty("hibernate.hbm2ddl.auto","update"); //update Schema - don't do this in prod
         this.sessionFactory = configuration.buildSessionFactory();
