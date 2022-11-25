18d1636df370ded91ece8a5e252e1a07b4f19100
sebastian almfort <sebastian_almfort@hotmail.com>
2022-11-25 13:02:42 +0100

    Rodet lidt med filen. Ikke lavet nogle store ændringer

--- src/test/java/service/LoginTest.java
+++ src/test/java/service/LoginTest.java
@@ -3,38 +3,52 @@ package service;
 
 import DB.HibernateController;
 import DB.User;
-import com.fasterxml.jackson.annotation.JacksonInject;
+//import com.fasterxml.jackson.annotation.JacksonInject;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+//import org.hibernate.Transaction;
 //import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
 import org.hibernate.query.Query;
 import org.testng.annotations.Test;
 //import java.io.ObjectInputStream;
 import java.util.List;
 //import org.slf4j.impl.StaticLoggerBinder;
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 //import org.hibernate.SessionFactory;
 //import org.hibernate.cfg.Configuration;
-
 //import static org.testng.Assert.*;
+
 
 public class LoginTest {
 
     @Test
     public static void testGetUser() {
+
+        /*
+        int a, b, c;
+        a = 2;
+        b = 10;
+        c = a + b;
+        if(c == 12){
+            System.out.println("True");
+        }
+        else{
+            System.out.print("False");
+        }
+        */
+
         HibernateController hibernateController =
-                //new HibernateController("pgtest-db.caprover.grp1.diplomportal.dk:6543/pg");
-                new HibernateController("test-db.prover.grp1.diplomata.dk:6543/pg");
+                new HibernateController("pgtest-db.caprover.grp1.diplomportal.dk:6543/pg");
+                //new HibernateController("test-db.prover.grp1.diplomata.dk:6543/pg");
         SessionFactory sessionFactory = hibernateController.getSessionFactory();
         Session session = sessionFactory.openSession();
 
-        JacksonInject.Value user;
+        //JacksonInject.Value user;
 
-        Transaction readTransaction = session.beginTransaction();
+        //Transaction readTransaction = session.beginTransaction();
 
         CriteriaBuilder cb = session.getCriteriaBuilder();
         CriteriaQuery<User> cr = cb.createQuery(User.class);
@@ -42,13 +56,13 @@ public class LoginTest {
         cr.select(root);
         /* Skriv hvilken user der skal findes */
         //String usertoFind = "Melman";
-        String usertoFind = "Mel-man";
+        String usertoFind = "Melman";
         cr.select(root).where(cb.like(root.get("username"), usertoFind));
 
         Query<User> query = session.createQuery(cr);
         List<User> results = query.getResultList();
 
-        if(results.equals("Mel-man")){
+        if(results.toString().equals("Mel-man")){
         //if(results.toString().equals("Melman")){
             System.out.println("True");
         }
