388cb25bf57f05387692315b73d2e65fe4c41df7
HildibjørgDidriksen <s164539@student.dtu.dk>
2022-11-28 08:53:18 +0100

    Updated the QuizService to create a quiz and to update the quiz with questions

--- src/main/java/service/QuizService.java
+++ src/main/java/service/QuizService.java
@@ -2,25 +2,49 @@ package service;
 
 import DB.HibernateController;
 import DB.Quiz;
+import DB.Question;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.criteria.JpaCriteriaQuery;
 
 import java.util.List;
 
 
 @Produces(MediaType.APPLICATION_JSON)
-@Path("quizzes")
+@Path("quiznew")
 public class QuizService {
     private static final SessionFactory sessionFactory = new HibernateController("pgtest-db.caprover.grp1.diplomportal.dk:6543/pg").getSessionFactory();
 
     @POST
     public int createQuiz(Quiz quiz){
         Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
         session.persist(quiz);
+        transaction.commit();
+
         return quiz.getId();
+    }
+
+    @POST
+    @Path("/questions/{id}")
+    public int addQuestions(@PathParam("id") int quizId, List<Question> questionList){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Quiz quiz = session.get(Quiz.class, quizId);
+        quiz.setQuestionsList(questionList);
+        session.persist(quiz);
+        transaction.commit();
+
+        return quiz.getId();
+    }
+
+    @GET
+    @Path("/questions/{id}")
+    public int getTest(@PathParam("id") int quizId){
+        return quizId;
     }
 
     @GET
