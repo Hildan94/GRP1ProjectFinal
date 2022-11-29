e9887b030d6289288bcd8fa7f217f600d3e179e3
sebastian almfort <sebastian_almfort@hotmail.com>
2022-11-29 22:06:26 +0100

    Igang med at få sat login op sammen med dev branchen. Igang med at kigge på TokenStore.js

--- web/src/pages/frontpage.js
+++ web/src/pages/frontpage.js
@@ -1,59 +1,82 @@
+
+
+
+
+
 import './../Backend/frontpage.css';
 import profile from "./../image/a.png";
 import email from "./../image/email.png";
 import pass from "./../image/pass.png";
 import {useNavigate} from "react-router-dom";
 import {Helmet} from "react-helmet";
+import React, {useState} from 'react';
+
 
 import {tokenStore} from "../stores/TokenStore";
 
 function Frontpage() {
 
+    const [Username, setUsername] = useState('Username');
+    const [Password, setPassword] = useState('Password');
+
     const navigate = useNavigate()
 
-    const toHome = () => {
-        navigate('/home')
+    const toThirdpartLogin = () => {
+        navigate('/campuslogin')
     }
 
-        return (
-            <div className="main">
-                <Helmet>
-                    <title>NEM Læringsplatform</title>
-                </Helmet>
-                <div className="sub-main">
-                    <div>
-                        <div className="img">
-                            <div className="container-image">
-                                <img src={profile} alt="profile" className="profile"/>
-
-                            </div>
-
-                        </div>
-                        <div>
-                            <h1>Loginside</h1>
-                            <div>
-                                <img src={email} alt="email" className="email"/>
-                                <input type="text" placeholder="Brugernavn" className="name"/>
-                            </div>
-                            <div className="second-input">
-                                <img src={pass} alt="pass" className="email"/>
-                                <input type="password" placeholder="Kodeord" className="name"/>
-                            </div>
-                            <div className="login-button">
-                                <button onClick={toHome}>Log ind</button>
-                                <button onClick={()=>tokenStore.doLogin()}>Log ind</button>
-                            </div>
-
-                            <p className="link">
-                                <a href="#">Glemt kodeord?</a> Or <a href="#">Opret bruger</a>
-                            </p>
-
+    return (
+        <div className="main">
+            <Helmet>
+                <title>NEM Læringsplatform</title>
+            </Helmet>
+            <div className="sub-main">
+                <div>
+                    <div className="img">
+                        <div className="container-image">
+                            <img src={profile} alt="profile" className="profile"/>
                         </div>
                     </div>
+                    <div>
 
+                        <h1>Loginside</h1>
+                        <div>
+                            <img src={email} alt="email" className="email"/>
+                            <input
+                                type="text"
+                                name="Brugernavn"
+                                required
+                                onChange={(e) => setUsername(e.target.value)}>
+                            </input>
+
+                        </div>
+                        <div className="second-input">
+
+                            <img src={pass} alt="pass" className="email"/>
+                            <input
+                                type="text"
+                                name="Kodeord"
+                                required
+                                onChange={(e) => setPassword(e.target.value)}>
+                            </input>
+
+                        </div>
+                        <div className="login-button">
+
+                            <button onClick={()=>tokenStore.doLogin(Username,Password)}>Log ind</button>
+                            <button onClick={toThirdpartLogin}>Trejdepartslogin </button>
+                        </div>
+
+                        <p className="link">
+                            <a href="#">Glemt kodeord</a> Or <a href="/#/signup">Opret bruger </a>
+                        </p>
+
+                    </div>
                 </div>
+
             </div>
-        );
-    }
+        </div>
+    );
+}
 
 export default Frontpage;
